79d6000bc0b7489b3b7dc3cfc0dcc90577760c27
admin <kimhjona@gmail.com>
2017-02-27 15:30:19 -0800

    getting it working

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = 3000;
+
+//this isn't working for some reason. too lazy to solve right now so did direct request below.
+app.use(express.static(path.join(__dirname, 'dejavue')));
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dejavue/app/index.html'));
+});
+
+app.get('/dejavue/assets/js/vue.js', function(req, res) {
+  res.sendFile(path.join(__dirname, '/dejavue/assets/js/vue.js'));
+});
+
+app.listen(PORT, function () {
+   console.log("...listening on port", PORT);
+});
+
+
+function createDV() {
+
+    // gets document.body's child nodes which are direct child html elements
+  const keysArray = Object.keys(document.body.children);
+  
+  console.log('keys', keysArray)
+    // initialize empty array to store root node objects
+      const rootNodes = [];
+
+      function findRoots() {
+
+    // iterate through keysArray to push only Vue root nodes into rootNodes array
+        for (let i = 0; i < keysArray.length; i += 1) {
+          const testNode = document.body.children[keysArray[i]];
+        console.log('test node for root value', testNode)    
+        if (!rootNodes.includes(testNode) &&  `testNode.__vue__`) rootNodes.push(testNode);
+      }
+
+        return rootNodes;
+      };
+
+      const roots = findRoots();
+      console.log('rootNodes', rootNodes);
+}
+
+console.log(createDV());
